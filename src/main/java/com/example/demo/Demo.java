be50ac53d55ec63535d8ab7f3dfd2a6992ceede9
Kiran <kiran@gmail.com>
2025-05-17 23:53:07 +0530

    Added @SpringBootApplication to fix Spring context error

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -1,24 +1,23 @@
 package com.example.demo;
 
-import java.util.*;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import jakarta.annotation.PostConstruct;
-
+@SpringBootApplication
 public class Demo {
-	
-	public static Logger logger=LoggerFactory.getLogger(Demo.class);
-	
-	@PostConstruct
-	public void intt() {
-		logger.info("Application started....");
-	}
-	public static void main(String[] args) {
-		
-		logger.info("Application exceuted....");
 
-	
-}
+    public static Logger logger = LoggerFactory.getLogger(Demo.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("Application started....");
+    }
+
+    public static void main(String[] args) {
+        logger.info("Application executed....");
+        SpringApplication.run(Demo.class, args);
+    }
 }
